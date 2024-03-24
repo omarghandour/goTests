72047d07330a2bfb1d183438a5270ff54f54b95d
omarghandour <alex.forget50@gmail.com>
2024-03-24 19:27:01 +0200

    hi

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -144,12 +144,18 @@ type Bitcoin int
 type Wallet struct {
     balance Bitcoin
 }
+type Stringer interface {
+	String() string
+}
 func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
     w.balance += amount
 }
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 func TestWallet(t *testing.T){
