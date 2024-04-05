932e17cdcf93b3b55f2dded5eb42454e877e5789
Omar Ghandour <109479370+omarghandour@users.noreply.github.com>
2024-04-05 23:25:05 +0200

    Update hello_test.go

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -29,6 +29,6 @@ t.Run("in French", func(t *testing.T) {
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want  %q", got, want)
 	}
 }
